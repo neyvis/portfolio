39e21d4720883c1446eaa80d8ce5cda92d673e58
neyvis <neyvis.remon.gonzalez@gmail.com>
2017-09-24 14:47:39 -0400

    Adding my portfolio app.

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'g(km-1*l7m+iuu+cndsy4kfgzpnpxmc@68v+(&7eafv#-02$ds'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'neyvis.pythonanywhere.com']
 
 
 # Application definition
@@ -118,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
